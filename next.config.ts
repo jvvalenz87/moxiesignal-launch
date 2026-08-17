e3e330a10c204c494616826723c798804a78bd42
jvvalenz87 <jvvalenz@asu.edu>
2026-08-17 11:34:16 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,20 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
   },
+  async redirects() {
+    return [
+      {
+        source: "/privacy.html",
+        destination: "/privacy",
+        permanent: true,
+      },
+      {
+        source: "/terms.html",
+        destination: "/terms",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
